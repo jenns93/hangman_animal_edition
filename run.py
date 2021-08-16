c19e5620e8c3382cd1262b02c0bd1c63d953ba62
jenns93 <jjenns93@gmail.com>
2021-08-16 02:08:18 +0000

    Add input_checker to validate user input into guess_data_lists to give results

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+import random
 word_dic = {
     "easy": [
         "cat",
@@ -91,6 +91,42 @@ def difficulty_selection():
         select_difficulty = input("Choose a difficulty easy/med/hard:\n")
     else:
         print(f"You selected: {select_difficulty}")
+        word_to_guess = guess_data_lists["word_to_guess"]
+        word_to_guess = random.choice(word_dic[select_difficulty])
+        input_checker(word_to_guess)
+
+
+def input_checker(word_to_guess):
+    """
+    Updates correct/inccorect letter lists and prints the main body of the game
+     (hangman picture/word to guess/wrong answers)
+    """
+    i = 0
+    set_of_word = set(word_to_guess)
+    length_of_set = len(set_of_word)
+    while i < 9:
+        letter_input = input("Please enter a letter: ")
+        if not letter_input.isalpha() or len(letter_input) > 1:
+            print("Invalid! Please try again:")
+        elif letter_input in guess_data_lists["correct"]:
+            print(f"You have already tried: {letter_input}")
+        else:
+            if letter_input in word_to_guess:
+                print(f"correct {letter_input} is in the word \n")
+                if letter_input not in guess_data_lists["correct"]:
+                    guess_data_lists["correct"].append(letter_input)
+                if length_of_set == len(guess_data_lists["correct"]):
+                    print(f"!! WINNER !!\n the word was {word_to_guess}")
+            else:
+                print(f"unlucky the word does not contain {letter_input}\n")
+                if letter_input not in guess_data_lists["wrong"]:
+                    guess_data_lists["wrong"].append(letter_input)
+                    i += 1
+                elif letter_input in guess_data_lists["wrong"]:
+                    print(f"You have already tried: {letter_input}")
+                print(f"wrong answers {i}")
+            if i >= 9:
+                print(f"!! GAME OVER !!\nthe word was\n{word_to_guess}")
 
 
 title_text()
